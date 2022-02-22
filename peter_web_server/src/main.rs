679af10fc5afb1b5762c744b5d645f4ade7f0df0
PeterWright <peterwrighten@gmail.com>
2022-02-22 12:42:27 +0900

    update::single_thread

--- peter_web_server/src/main.rs
+++ peter_web_server/src/main.rs
@@ -13,9 +13,14 @@ fn main() {
     fn handle_stream(mut stream: TcpStream) {
         let mut buffer = [0; 512];
         stream.read(&mut buffer).unwrap();
-
-        let contents = fs::read_to_string("peter.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        let get = b"GET / HTTP/1.1\r\n";
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "peter.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+        let contents = fs::read_to_string(filename).unwrap();
+        let response = format!("{}{}", status_line, contents);
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
